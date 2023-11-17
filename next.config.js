c6e57b4a1169ac3a614769e27ad3e5d8d2e6c4ac
Brendan Smith <44657354+brendanrsmith@users.noreply.github.com>
2023-11-17 14:52:36 -0800

    Update pages.yml for next 14 (#106)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-// This file is not going through babel transformation.
-// So, we write it in vanilla JS
-// (But you could use ES2015 features supported by your Node.js version)
-
 const debug = process.env.NODE_ENV !== "production";
 
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   assetPrefix: !debug ? "/" : "",
+  output: "export",
+  images: { unoptimized: true },
 };
+
+module.exports = nextConfig;
